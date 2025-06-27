f3905d46fd67e3db377ebde27be169e25b75336d
EliasSuez <48240167@est.ort.edu.ar>
2025-06-27 08:13:59 -0300

    .env

--- src/configs/db-configs.js
+++ src/configs/db-configs.js
@@ -1,10 +1,11 @@
+import ".env";
+
 const config = {
-    host : "localhost",
-    database : "Alumnos",
-    user : "postgres",
-    password : "root",
-    port : "5432"
+  host: process.env.DB_HOST ?? "",
+  database: process.env.DB_DATABASE ?? "",
+  user: process.env.DB_USER ?? "",
+  password: process.env.DB_PASSWORD ?? "",
+  port: process.env.DB_ROOT ?? 5432,
+};
 
-}
-
-export default config
+export default config;
